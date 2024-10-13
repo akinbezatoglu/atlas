7f06907a2a27a467bb75bac80837034b76abc80f
akinbezatoglu <akinbezatoglu@gmail.com>
2024-10-13 23:53:57 +0300

    feat(auth): complete auth logic

---
+++ src/features/auth/components/sign-in-card.tsx
@@ -0,0 +1,134 @@
+'use client'
+
+import Link from 'next/link';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+import { useForm } from "react-hook-form";
+
+import { DottedSeparator } from "@/components/dotted-separator";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+import { loginSchema } from '../schemas';
+import { useLogin } from '../api/use-login';
+import { Label } from '@/components/ui/label';
+
+export const SignInCard = () => {
+  const { mutate, isPending } = useLogin();
+
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof loginSchema>) => {
+    mutate({ json: values });
+  };
+
+  return (
+    <Card className="w-full h-full md:w-[487px] border-none shadow-none">
+      <CardHeader className="flex items-center justify-center text-center p-7">
+        <CardTitle className="text-2xl">
+          Welcome back!
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="p-7">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              name="email"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <Label>Email*</Label>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="email"
+                      placeholder="you@example.com"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <Label>Password*</Label>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="password"
+                      placeholder="●●●●●●●●"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button
+              size="lg"
+              disabled={isPending}
+              className="w-full"
+            >
+              Login
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
+      <div className="px-7">
+        <DottedSeparator></DottedSeparator>
+      </div>
+      <CardContent className="p-7 flex flex-col gap-y-4">
+        <Button
+          variant="secondary"
+          size="lg"
+          disabled={isPending}
+          className="w-full"
+        >
+          <FcGoogle className="mr-2 size-5" />
+          Login with Google
+        </Button>
+        <Button
+          variant="secondary"
+          size="lg"
+          disabled={isPending}
+          className="w-full"
+        >
+          <FaGithub className="mr-2 size-5" />
+          Login with GitHub
+        </Button>
+      </CardContent>
+      <CardContent className="p-7 flex items-center justify-center">
+        <p>
+          Don&apos;t have an account?
+          <Link href="/sign-up">
+            <span className="text-blue-700">&nbsp;Sign Up</span>
+          </Link>
+        </p>
+      </CardContent>
+    </Card>
+  );
+};
---
+++ src/features/auth/constants.ts
@@ -0,0 +1,1 @@
+export const AUTH_COOKIE = "atlas_session"
